67aecd80b5b9a2f59be4043daa7f8f479d526268
Ganesh SHelke <ganeshshelke0833@gmail.com>
2025-03-07 11:13:10 +0530

    Add

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ DATABASE_URL = "mysql+pymysql://root:MahitNahi%4012@localhost/sentiment_db"
 
 
 # Create an engine
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 # Create a session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
